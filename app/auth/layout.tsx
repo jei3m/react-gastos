2bb4df7d89ce0ee19ebf52e2e0d8d5395155b525
Justin Miguel Reyes <virozzegt@gmail.com>
2025-08-24 10:28:05 +0800

    feat: auth layout

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -1,26 +1,9 @@
-import type { Metadata } from "next";
-import { Poppins } from "next/font/google"; // Already imported
-import "../globals.css";
-
-const poppins = Poppins({
-  subsets: ["latin"],
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-  variable: "--font-poppins",
-});
-
-export const metadata: Metadata = {
-  title: "Gastos",
-  description: "Finance Tracker",
-};
-
-export default function RootLayout({
+export default function AuthLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en">
-      <body className={`${poppins.variable} font-sans antialiased`}>
-        {children}
-      </body>
-    </html>
+    <>
+      {children}
+    </>
   );
-}
+};
