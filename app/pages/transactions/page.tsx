818bab5321b48c3a5bfa5b6bd21ba7d01b8908af
Justin Miguel Reyes <virozzegt@gmail.com>
2025-08-26 04:58:20 +0800

    ref: used tailwind animations; remove outer div

--- app/pages/transactions/page.tsx
+++ app/pages/transactions/page.tsx
@@ -154,180 +154,183 @@ export default function Transactions() {
   }, [activeTab])
 
   return (
-    <>
+    <main className='flex flex-col space-y-2 min-h-screen pb-12'>
       {/* Date Card Section */}
-      <main className='flex flex-col m-auto space-y-2 min-h-screen pb-12'>
-        {isScrolled ?         
-          <section className='sticky top-0 z-10 transition-all'>
-            <Card className="-mt-2 w-full border-0 rounded-none">
-              <CardHeader
-                className='flex
-                flex-col 
-                justify-center 
-                items-center'
-              >
-                {/* Tabs Selection */}
-                <div className='flex items-center gap-x-2'>
-                  <Calendar />
-                  <Tabs defaultValue='daily' value={activeTab} onValueChange={setActiveTab}>
-                    <TabsList className='bg-white'>
-                      {tabItems.map((item, index) => (
-                        <TabsTrigger
-                          value={item.value}
-                          key={index}
-                        >
-                          {/* Capitalized first letter of item.value */}
-                          {item.value.charAt(0).toUpperCase() + item.value.slice(1)}
-                        </TabsTrigger>
-                      ))}
-                    </TabsList>
-                  </Tabs>
-                </div>
+      {isScrolled ?         
+        <section 
+          className='sticky top-0 z-10 
+          transition-all duration-150 
+          ease-in-out'
+        >
+          <Card className="-mt-2 w-full border-0 rounded-none">
+            <CardHeader
+              className='flex
+              flex-col 
+              justify-center 
+              items-center'
+            >
+              {/* Tabs Selection */}
+              <div className='flex items-center gap-x-2'>
+                <Calendar />
+                <Tabs defaultValue='daily' value={activeTab} onValueChange={setActiveTab}>
+                  <TabsList className='bg-white'>
+                    {tabItems.map((item, index) => (
+                      <TabsTrigger
+                        value={item.value}
+                        key={index}
+                      >
+                        {/* Capitalized first letter of item.value */}
+                        {item.value.charAt(0).toUpperCase() + item.value.slice(1)}
+                      </TabsTrigger>
+                    ))}
+                  </TabsList>
+                </Tabs>
+              </div>
 
-                {/* Date Display and Date Change */}
-                <div className='w-full'>
-                  <div className="flex 
-                      justify-between 
-                      items-center
-                      font-semibold"
-                  >
-                    <ChevronLeft
-                      className='cursor-pointer'
-                      onClick={() => handleDateChange('prev')}
-                    />
-                    {dateDisplay}
-                    <ChevronRight
-                      className='cursor-pointer'
-                      onClick={() => handleDateChange('next')}
-                    />
-                  </div>
+              {/* Date Display and Date Change */}
+              <div className='w-full'>
+                <div className="flex 
+                    justify-between 
+                    items-center
+                    font-semibold"
+                >
+                  <ChevronLeft
+                    className='cursor-pointer'
+                    onClick={() => handleDateChange('prev')}
+                  />
+                  {dateDisplay}
+                  <ChevronRight
+                    className='cursor-pointer'
+                    onClick={() => handleDateChange('next')}
+                  />
                 </div>
-              </CardHeader>
-            </Card>
-            <div className='w-full border-t-2 border-black' />
-          </section>
-          :
-          <section className='pt-2 px-3 transition-all'>
-            <Card className="mt-0 w-full border-2 ">
-              <CardHeader
-                className='flex
-                flex-col 
-                justify-center 
-                items-center'
-              >
-                {/* Tabs Selection */}
-                <div className='flex items-center gap-x-2'>
-                  <Calendar />
-                  <Tabs defaultValue='daily' value={activeTab} onValueChange={setActiveTab}>
-                    <TabsList className='bg-white'>
-                      {tabItems.map((item, index) => (
-                        <TabsTrigger
-                          value={item.value}
-                          key={index}
-                        >
-                          {/* Capitalized first letter of item.value */}
-                          {item.value.charAt(0).toUpperCase() + item.value.slice(1)}
-                        </TabsTrigger>
-                      ))}
-                    </TabsList>
-                  </Tabs>
+              </div>
+            </CardHeader>
+          </Card>
+          <div className='w-full border-t-2 border-black' />
+        </section>
+        :
+        <section 
+          className='pt-2 px-3 
+          transition-all duration-150 
+          ease-in-out'
+        >
+          <Card className="mt-0 w-full border-2 ">
+            <CardHeader
+              className='flex
+              flex-col 
+              justify-center 
+              items-center'
+            >
+              {/* Tabs Selection */}
+              <div className='flex items-center gap-x-2'>
+                <Calendar />
+                <Tabs defaultValue='daily' value={activeTab} onValueChange={setActiveTab}>
+                  <TabsList className='bg-white'>
+                    {tabItems.map((item, index) => (
+                      <TabsTrigger
+                        value={item.value}
+                        key={index}
+                      >
+                        {/* Capitalized first letter of item.value */}
+                        {item.value.charAt(0).toUpperCase() + item.value.slice(1)}
+                      </TabsTrigger>
+                    ))}
+                  </TabsList>
+                </Tabs>
+              </div>
+
+              {/* Date Display and Date Change */}
+              <div className='w-full'>
+                <div className="flex 
+                    justify-between 
+                    items-center
+                    font-semibold"
+                >
+                  <ChevronLeft
+                    className='cursor-pointer'
+                    onClick={() => handleDateChange('prev')}
+                  />
+                  {dateDisplay}
+                  <ChevronRight
+                    className='cursor-pointer'
+                    onClick={() => handleDateChange('next')}
+                  />
                 </div>
+              </div>
+            </CardHeader>
 
-                {/* Date Display and Date Change */}
-                <div className='w-full'>
-                  <div className="flex 
-                      justify-between 
-                      items-center
-                      font-semibold"
-                  >
-                    <ChevronLeft
-                      className='cursor-pointer'
-                      onClick={() => handleDateChange('prev')}
-                    />
-                    {dateDisplay}
-                    <ChevronRight
-                      className='cursor-pointer'
-                      onClick={() => handleDateChange('next')}
-                    />
-                  </div>
-                </div>
-              </CardHeader>
+            {/* Line Separator */}
+            <div className='w-full border-t border-gray-300' />
 
-              {/* Line Separator */}
-              <div className='w-full border-t border-gray-300' />
-
-              <CardContent className='flex flex-col gap-y-4'>
+            <CardContent className='flex flex-col gap-y-4'>
+              <div className='flex flex-col'>
+                <h3 className='text-gray-600 font-normal text-lg'>
+                  Balance
+                </h3>
+                <h1 className='text-2xl font-extrabold'>
+                  PHP 1,200.00
+                </h1>
+              </div>
+              <div className='flex flex-row justify-between w-full'>
                 <div className='flex flex-col'>
-                  <h3 className='text-gray-600 font-normal text-lg'>
-                    Balance
-                  </h3>
-                  <h1 className='text-2xl font-extrabold'>
+                  <h4 className='text-gray-600 font-normal text-md'>
+                    Total Income
+                  </h4>
+                  <h1 className='text-xl font-bold text-green-500'>
                     PHP 1,200.00
                   </h1>
                 </div>
-                <div className='flex flex-row justify-between w-full'>
-                  <div className='flex flex-col'>
-                    <h4 className='text-gray-600 font-normal text-md'>
-                      Total Income
-                    </h4>
-                    <h1 className='text-xl font-bold text-green-500'>
-                      PHP 1,200.00
-                    </h1>
-                  </div>
-                  <div className='flex flex-col'>
-                    <h4 className='text-gray-600 font-normal text-md'>
-                      Total Expenses
-                    </h4>
-                    <h1 className='text-xl font-bold text-red-500'>
-                      PHP 1,200.00
-                    </h1>
-                  </div>
+                <div className='flex flex-col'>
+                  <h4 className='text-gray-600 font-normal text-md'>
+                    Total Expenses
+                  </h4>
+                  <h1 className='text-xl font-bold text-red-500'>
+                    PHP 1,200.00
+                  </h1>
                 </div>
-              </CardContent>
-            </Card>
-          </section>
-        }
+              </div>
+            </CardContent>
+          </Card>
+        </section>
+      }
 
-
-        {/* Transactions Section */}
-        <section className='flex flex-col space-y-2 px-3 mb-2'>
-          {transactions ? 
-            <>
-              {transactions.map((transaction, index) => (
-                <Card key={index} className='border-2'>
-                  <CardHeader>
-                    <CardTitle className='flex justify-between'>
-                      <span>{transaction.date}</span>
-                      <span className='text-red-500'>{transaction.total}</span>
-                    </CardTitle>
-                  </CardHeader>
-                  <div className='w-full border-t border-gray-300' />
-                  <CardContent className='-mb-4'>
-                    {transaction.details.map((detail, index) => (
-                      <div key={index} className='space-y-3 flex flex-row items-center justify-between'>
-                        <div className='flex flex-col text-sm'>
-                          <span>
-                            {detail.category}
-                          </span>
-                          <span className='text-gray-600'>
-                            {detail.note}
-                          </span>
-                        </div>
-                        <span className='text-sm text-red-500'>
-                          {detail.amount}
+      {/* Transactions Section */}
+      <section className='flex flex-col space-y-2 px-3 mb-2'>
+        {transactions && ( 
+          <>
+            {transactions.map((transaction, index) => (
+              <Card key={index} className='border-2'>
+                <CardHeader>
+                  <CardTitle className='flex justify-between'>
+                    <span>{transaction.date}</span>
+                    <span className='text-red-500'>{transaction.total}</span>
+                  </CardTitle>
+                </CardHeader>
+                <div className='w-full border-t border-gray-300' />
+                <CardContent className='-mb-4'>
+                  {transaction.details.map((detail, index) => (
+                    <div key={index} className='space-y-3 flex flex-row items-center justify-between'>
+                      <div className='flex flex-col text-sm'>
+                        <span>
+                          {detail.category}
+                        </span>
+                        <span className='text-gray-600'>
+                          {detail.note}
                         </span>
                       </div>
-                    ))}
-                  </CardContent>
-                </Card>
-              ))}
-            </>
-            :
-            <></>
-          }
+                      <span className='text-sm text-red-500'>
+                        {detail.amount}
+                      </span>
+                    </div>
+                  ))}
+                </CardContent>
+              </Card>
+            ))}
+          </>
+        )}
 
-        </section>
-      </main>
-    </>
+      </section>
+    </main>
   );
 };
