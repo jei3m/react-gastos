6cfa17e474b608db62e88ab7f3a2672d11e64572
Justin Miguel Reyes <virozzegt@gmail.com>
2025-08-30 17:01:41 +0800

    ref: development mode

--- next.config.ts
+++ next.config.ts
@@ -5,8 +5,11 @@ const nextConfig: NextConfig = {
   /* your regular Next.js options */
 };
 
+const isDev = process.env.NODE_ENV === 'development';
+
 export default withPWA({
   dest: 'public',
+  disable: isDev,
   workboxOptions: {
     mode: 'production',   // disables workbox logging
   },
